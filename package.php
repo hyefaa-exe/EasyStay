65a3f4017add66daf6ef7f13af5a2a056036d9d2
hyefaa-exe <fasihahzamri77@gmail.com>
2026-06-17 15:35:47 +0800

    Fix Chalet Package image extension to .png in package.php and book_new.php

--- package.php
+++ package.php
@@ -571,7 +571,7 @@ $result = $stmt->get_result();
                                          <?php
                                          $pkg_image = '';
                                          if ($row['package_id'] == 12) {
-                                             $pkg_image = 'img/chalet night.jpg';
+                                             $pkg_image = 'img/chalet_night_hd.png';
                                          } elseif ($row['package_id'] == 13) {
                                              $pkg_image = 'img/homestay day.jpg';
                                          } elseif ($row['package_id'] == 14) {
